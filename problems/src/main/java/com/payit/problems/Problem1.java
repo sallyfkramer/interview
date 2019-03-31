75f0d76118c17671d4589a97e7514a96f7dad135
Sarah Kramer <43766609+sallyfkramer@users.noreply.github.com>
2019-03-31 11:46:46 -0500

    Two solutions for problem 1 with explantions

--- problems/src/main/java/com/payit/problems/Problem1.java
+++ problems/src/main/java/com/payit/problems/Problem1.java
@@ -8,8 +8,42 @@ import java.util.List;
  * For indexes that are multiples of five, the value should be "Buzz". For indexes that are multiples of both three and five, the value
  * should be "FizzBuzz".
  */
-public class Problem1 {
+public class Problem1{
+
+/**   This fizzBuzz returns the list with all the original values unless they were in a location
+ *   that caused them to be replaced by fizzBuzz
+ */
     public List<String> fizzBuzz(List<String> fizzBuzz) {
+        int i = 0;
+        for (String item : fizzBuzz) {
+            if (i % 15 == 0) {
+                fizzBuzz.set(i, "FizzBuzz");
+            } else if (i % 5 == 0) {
+                fizzBuzz.set(i, "Buzz");
+            } else if (i % 3 == 0) {
+                fizzBuzz.set(i, "Fizz");
+            }
+            i += 1;
+        }
+        return fizzBuzz;
+    }
+
+/** This fizzBuzz returns a list with all of the values replaced by either an integer or fizzBuzz.
+  */
+    public List<String> otherFizzBuzz(List<String> fizzBuzz) {
+        int i = 0;
+        for (String item : fizzBuzz) {
+            if (i % 15 == 0) {
+                fizzBuzz.set(i, "FizzBuzz");
+            } else if (i % 5 == 0) {
+                fizzBuzz.set(i, "Buzz");
+            } else if (i % 3 == 0) {
+                fizzBuzz.set(i, "Fizz");
+            } else {
+                fizzBuzz.set(i, Integer.toString(i));
+                i += 1;
+            }
+        }
         return fizzBuzz;
     }
 }
